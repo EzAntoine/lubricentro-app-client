d49c653bc3320fb727ed129b65ccb3f007837c06
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-20 23:24:33 -0300

    feat: About Us - Structure

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -1,14 +1,19 @@
 export default function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel massa
-        vel nisi finibus mollis. Maecenas vel turpis id neque efficitur
-        ullamcorper. Nulla facilisi. Sed vel finibus lectus, in pharetra neque.
-        Integer et ipsum in ipsum auctor dictum. Donec sed turpis ac ipsum
-        tincidunt fermentum. Nulla facilisi.
-      </p>
-    </div>
+    <section className="text-gray-300" id="aboutus">
+      <div>
+        <h2 className="text-left text-4xl font-bold text-white mt-4 mb-4 md:mb-12">
+          Sobre Nosotros
+        </h2>
+        <h2 className="">About Us</h2>
+        <p className="text-[#a8aeb3] text-base sm:text-lg mb-6 lg:text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel massa
+          vel nisi finibus mollis. Maecenas vel turpis id neque efficitur
+          ullamcorper. Nulla facilisi. Sed vel finibus lectus, in pharetra
+          neque. Integer et ipsum in ipsum auctor dictum. Donec sed turpis ac
+          ipsum tincidunt fermentum. Nulla facilisi.
+        </p>
+      </div>
+    </section>
   );
 }
