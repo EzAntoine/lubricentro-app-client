cb48298a990df72d70ceb77d1b337e146adc9055
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-22 19:07:47 -0300

    Contact structure

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,11 +1,26 @@
 export default function Contact() {
   return (
-    <section className="text-gray-300 w-full p-8" id="contact">
-      <div>
-        <h2 className="text-left text-4xl font-bold text-white mt-8 mb-8 md:mb-12">
+    <section id="contact">
+      <div className="w-full p-8 bg-[#2ccc71] bg-opacity-20">
+        <h2 className="text-left text-4xl font-bold text-white md:mb-12">
           Contacto
         </h2>
-        <p>Hola soy el contacto</p>
+        <div className="text-gray-300 font-bold text-base sm:text-lg mb-6 lg:text-xl px-24 text-balance">
+          <span>Escribinos por Whatsapp BOTON</span>
+          <h3 className="text-2xl mt-4">Redes Sociales</h3>
+          <div className="container mx-auto flex flex-col items-center">
+            <ul className="flex space-x-4">
+              <li>LOGO lubricentro@gmail.com</li>
+              <li>LOGO @Instagram</li>
+              <li>LOGO TikTok</li>
+            </ul>
+          </div>
+          <h3 className="text-2xl mt-4">Ubicacion</h3>
+          <h4 className="text-center text-xl">
+            Calle 123, Bahia Blanca (B8000), Buenos Aires, Argentina
+          </h4>
+          <div>MAPA DE GOOGLE CON UBICACION</div>
+        </div>
       </div>
     </section>
   );
