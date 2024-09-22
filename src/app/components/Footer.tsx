264c56d650440526bb3a17a8d7fd136ba6e1d7f3
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-22 18:49:15 -0300

    Footer structure

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,14 @@
 export default function Footer() {
   return (
-    <section className="text-gray-300 w-full p-8 mt-2 bottom-0 right-0 left-0 bg-[#2d2c2d] bg-opacity-100">
-      <div>
-        <h2 className="text-left text-4xl font-bold text-white mb-4 md:mb-12">
-          Footer
-        </h2>
-        <p>Redes.</p>
+    <section className="text-gray-300 w-full p-2 bottom-0 right-0 left-0 bg-[#2d2c2d] bg-opacity-100">
+      <div className="container mx-auto flex flex-col items-center">
+        <ul className="flex space-x-4 mb-4 text-lg">
+          <li>LOGO lubricentro@gmail.com</li>
+          <li>LOGO +54291xxxxxxx</li>
+          <li>LOGO @Instagram</li>
+          <li>LOGO TikTok</li>
+        </ul>
+        <span className="text-sm">Sitio creado por EzAntoine</span>
       </div>
     </section>
   );
