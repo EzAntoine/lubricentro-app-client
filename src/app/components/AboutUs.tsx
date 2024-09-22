c08b2f20114df02831d50c5b2370aba53d9cff72
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-22 20:28:05 -0300

    About: id in section

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -1,7 +1,7 @@
 export default function AboutUs() {
   return (
-    <section>
-      <div className="w-full p-16 bg-[#e94c3d] bg-opacity-20" id="aboutus">
+    <section id="aboutus">
+      <div className="w-full p-16 bg-[#e94c3d] bg-opacity-20">
         <h2 className="text-left text-4xl font-bold text-white md:mb-12">
           Sobre Nosotros
         </h2>
