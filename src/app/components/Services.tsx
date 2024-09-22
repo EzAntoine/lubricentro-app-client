15d16676c90339bc7826034a502982a2e39b4b83
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-09-22 18:48:14 -0300

    Services structure

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,15 +1,18 @@
 export default function Services() {
   return (
-    <section className="text-gray-300 w-full p-8" id="services">
-      <div>
-        <h2 className="text-left text-4xl font-bold text-white mt-8 mb-8 md:mb-12">
+    <section id="services">
+      <div className="w-full p-8 bg-gray-400 bg-opacity-20">
+        <h2 className="text-left text-4xl font-bold text-white md:mb-12">
           Servicios
         </h2>
-        <div className="text-[#a8aeb3] text-base sm:text-lg mb-6 lg:text-xl px-24 text-balance">
-          <li>Servicios.</li>
-        </div>
+        <ul className="text-gray-300 text-base font-bold text-center sm:text-lg mb-6 lg:text-2xl px-24 text-balance">
+          <li>Service</li>
+          <li>Tren delantero</li>
+          <li>Cambio de aceite y lubricante</li>
+          <li>Liquido de frenos</li>
+        </ul>
       </div>
-      <div className="border-t border-gray-400 h-1 w-2/3 mt-8 mx-auto"></div>
+      <div className="border-t border-gray-400 h-1 w-2/3 m-8 mx-auto"></div>
     </section>
   );
 }
