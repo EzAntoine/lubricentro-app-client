a2b06cef98cdec25b4db4218b15e9eb60d8d85f2
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-10-15 10:17:37 -0300

    Dashboard: Background cover fixed

--- src/app/components/Dashboard/Dashboard.tsx
+++ src/app/components/Dashboard/Dashboard.tsx
@@ -30,7 +30,7 @@ export default function Dashboard({
   const renderContent = () => {
     switch (activeTab) {
       case "orders":
-        return <OrdersComponent data={data} />;
+        return <OrdersComponent />;
       case "clients":
         return <ClientsComponent />;
       case "users":
@@ -53,16 +53,18 @@ export default function Dashboard({
 
   return (
     <div className="bg-hero-image min-h-screen bg-cover bg-center bg-fixed">
-      <NavbarAdmin logout={logout} />
-      <div className="flex flex-grow mt-14">
-        <div className="mt-14 fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-10;">
-          <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
-        </div>
-        <main className="ml-64 h-screen flex-grow py-2 mb-2 min-h-screen bg-[#2d2c2d] bg-opacity-70">
-          <div className="bg-gray-400 bg-opacity-20 divide-y divide-gray-200">
-            {renderContent()}
+      <div className="bg-[#2d2c2d] bg-opacity-70">
+        <NavbarAdmin logout={logout} />
+        <div className="flex flex-grow mt-14">
+          <div className="mt-14 fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-10;">
+            <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
           </div>
-        </main>
+          <main className="ml-64 h-screen flex-grow py-2 mb-2 min-h-screen">
+            <div className="bg-gray-400 bg-opacity-20 divide-y divide-gray-200">
+              {renderContent()}
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
