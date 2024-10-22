bfb60079c0aabbc1182bacba96bffdf650fcf9bc
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-10-22 12:27:08 -0300

    ClientsComponent: SortButton

---
+++ src/app/components/Buttons/SortButton.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const SortButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative inline-block text-left">
+      {/* Botón para abrir/cerrar dropdown */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-1 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:black"
+      >
+        Ordenar
+        {/* Ícono de ordenamiento (puedes cambiarlo según necesites) */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+          />
+        </svg>
+      </button>
+
+      {/* Dropdown */}
+      {isOpen && (
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div className="py-1" role="none">
+            <button
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-400 w-full text-left"
+              role="menuitem"
+              onClick={() => console.log("Ordenar A-Z")}
+            >
+              A-Z
+            </button>
+            <button
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-400 w-full text-left"
+              role="menuitem"
+              onClick={() => console.log("Fecha: Más reciente")}
+            >
+              Fecha: Más reciente
+            </button>
+            <button
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-400 w-full text-left"
+              role="menuitem"
+              onClick={() => console.log("Fecha: Más antiguo")}
+            >
+              Fecha: Más antiguo
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SortButton;
--- src/app/components/TabComponents/ClientsComponent.tsx
+++ src/app/components/TabComponents/ClientsComponent.tsx
@@ -1,7 +1,9 @@
 import { EyeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import CreateClientForm from "../Forms/CreateClientForm";
 import { URL } from "../../../../config/consts";
+import Loading from "../Loader/Loading";
+import SortButton from "../Buttons/SortButton";
 interface Client {
   _id: string;
   name: string;
@@ -50,75 +52,82 @@ const ClientsComponent = () => {
           fetchClients={fetchClients}
         />
       ) : (
-        <div>
-          <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-1 -mt-1">
-            <h1 className="text-xl font-bold p-2">Clientes</h1>
-            <button
-              onClick={clickHandler}
-              className="px-4 py-1.5 text-sm font-medium text-white bg-[#1a7742] rounded hover:bg-[#72241d]"
-            >
-              Nuevo Cliente
-            </button>
-            <input
-              type="text"
-              placeholder="Buscar..."
-              className="p-1 rounded"
-            />
-          </div>
-          {clients.length === 0 ? (
-            <p className="py-6 text-lg bg-[#2d2c2d] bg-opacity-80 text-center">
-              No hay clientes disponibles.
-            </p>
-          ) : (
-            <div className="overflow-y-auto max-h-[500px]">
-              <table className="w-full h-full divide-y divide-gray-200 bg-[#2d2c2d] bg-opacity-80">
-                <thead>
-                  <tr>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
-                      Nombre
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
-                      Telefono
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
-                      Email
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
-                      Detalles
-                    </th>
-                    <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"></th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 min-h-screen">
-                  {clients.map((item) => (
-                    <tr key={item._id}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {item.name + " " + item.surname}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {item.phone}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {item.email}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {item.detail}
-                      </td>
-                      <td className="flex justify-end mx-auto mr-4">
-                        <button className="mt-2 p-2 text-sm font-medium rounded bg-gray-50 text-black hover:bg-[#1a7742] hover:text-white">
-                          <EyeIcon className="w-5 h-5" />
-                        </button>
-                        <button className="mt-2 ml-2 p-2 text-sm font-medium rounded bg-gray-50 text-black hover:bg-[#1a7742] hover:text-white">
-                          <PencilSquareIcon className="w-5 h-5" />
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        <Suspense fallback={<Loading />}>
+          <div>
+            <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-1 -mt-1">
+              <h1 className="text-xl font-bold p-2">Clientes</h1>
+              <button
+                onClick={clickHandler}
+                className="px-4 py-1.5 text-sm font-medium text-white bg-[#1a7742] rounded hover:bg-[#72241d]"
+              >
+                Nuevo Cliente
+              </button>
+              <div className="flex items-center">
+                <div className="mr-2 rounded">
+                  <SortButton />
+                </div>
+                <input
+                  type="text"
+                  placeholder="Buscar..."
+                  className="p-1 rounded"
+                />
+              </div>
             </div>
-          )}
-        </div>
+            {clients.length === 0 ? (
+              <p className="py-6 text-lg bg-[#2d2c2d] bg-opacity-80 text-center">
+                No hay clientes disponibles.
+              </p>
+            ) : (
+              <div className="overflow-y-auto max-h-[500px]">
+                <table className="w-full h-full divide-y divide-gray-200 bg-[#2d2c2d] bg-opacity-80">
+                  <thead>
+                    <tr>
+                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                        Nombre
+                      </th>
+                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                        Telefono
+                      </th>
+                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                        Email
+                      </th>
+                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider">
+                        Detalles
+                      </th>
+                      <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-800 uppercase tracking-wider"></th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 min-h-screen">
+                    {clients.map((item) => (
+                      <tr key={item._id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.name + " " + item.surname}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.phone}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.email}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.detail}
+                        </td>
+                        <td className="flex justify-end mx-auto mr-4">
+                          <button className="mt-2 p-2 text-sm font-medium rounded bg-gray-50 text-black hover:bg-[#1a7742] hover:text-white">
+                            <EyeIcon className="w-5 h-5" />
+                          </button>
+                          <button className="mt-2 ml-2 p-2 text-sm font-medium rounded bg-gray-50 text-black hover:bg-[#1a7742] hover:text-white">
+                            <PencilSquareIcon className="w-5 h-5" />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
+        </Suspense>
       )}
     </>
   );
