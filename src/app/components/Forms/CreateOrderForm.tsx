726cc76de53dfa8fed460b3c27c8baa0710820ba
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-11-05 22:51:15 -0300

    CreateVehicle: Vehicle is an object in database

--- src/app/components/Forms/CreateOrderForm.tsx
+++ src/app/components/Forms/CreateOrderForm.tsx
@@ -180,158 +180,160 @@ export default function CreateOrderForm({ setFormOpen, fetchOrders }) {
 
   const plateSelectHandler = (e) => {
     const selectedPlate = e.target.value;
-    console.log(selectedPlate);
 
     setNewOrder({ ...newOrder, vehiclePlate: selectedPlate }); // Actualizar el newOrder con el clientId
   };
 
   return (
     <div className="-mt-10 flex flex-col items-center justify-center min-h-screen">
-      <div className="bg-[#2d2c2d] bg-opacity-100 p-6 rounded-lg shadow-md w-96">
+      <div className="bg-[#2d2c2d] bg-opacity-100 p-6 rounded-lg shadow-md w-200">
         <div className="flex flex-col items-center justify-center">
           <p className="mb-2 text-center">NUEVA ORDEN</p>
         </div>
-        <form onSubmit={submitHandler}>
-          <div className="mb-2">
-            <label
-              htmlFor="clientId"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Cliente:
-            </label>
-            <div className="w-full inline-flex items-center">
-              <select
-                className="w-full mt-1 py-2 px-3 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded"
-                onChange={clientSelectHandler}
+        <form
+          onSubmit={submitHandler}
+          className="grid grid-cols-2 gap-4 w-full p-4"
+        >
+          <div>
+            <div className="mb-2">
+              <label
+                htmlFor="clientId"
+                className="block text-sm font-medium text-gray-300"
               >
-                <option value="">Seleccionar cliente</option>
-                {clients.length === 0 ? (
-                  <option disabled>No hay clientes disponibles.</option>
-                ) : (
-                  clients.map((item) => (
-                    <option key={item._id} value={item._id}>
-                      {item.surname + " " + item.name}
-                    </option>
-                  ))
-                )}
-              </select>
-              {/* <button
-                className="h-10 w-10 flex items-center justify-center ml-2"
-                title="Nuevo cliente"
-              >
-                <PlusCircleIcon className="h-9 w-9 text-white" />
-              </button> */}
-            </div>
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="vehiclePlate"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Patente:
-            </label>
-            <div className="w-full inline-flex items-center">
-              <select
-                className="w-full mt-1 py-2 px-3 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded"
-                onChange={plateSelectHandler}
-              >
-                <option value="">Seleccionar vehículo</option>
-                {selectedClient ? (
-                  selectedClient.vehicles?.length === 0 ? (
-                    <option disabled>
-                      El cliente no tiene vehículos agregados.
-                    </option>
+                Cliente:
+              </label>
+              <div className="w-full inline-flex items-center">
+                <select
+                  className="w-full mt-1 py-2 px-3 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded"
+                  onChange={clientSelectHandler}
+                >
+                  <option value="">Seleccionar cliente</option>
+                  {clients.length === 0 ? (
+                    <option disabled>No hay clientes disponibles.</option>
                   ) : (
-                    selectedClient.vehicles[0]?.map((item) => (
-                      <option key={item.plate} value={item.plate}>
-                        {item.plate}
+                    clients.map((item) => (
+                      <option key={item._id} value={item._id}>
+                        {item.surname + " " + item.name}
                       </option>
                     ))
-                  )
-                ) : (
-                  <option disabled>No hay cliente seleccionado.</option>
-                )}
-              </select>
+                  )}
+                </select>
+                {/* <button
+                className="h-10 w-10 flex items-center justify-center ml-2"
+                title="Nuevo cliente"
+                >
+                <PlusCircleIcon className="h-9 w-9 text-white" />
+                </button> */}
+              </div>
+            </div>
+
+            <div className="mb-2">
+              <label
+                htmlFor="failure"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Falla:
+              </label>
+              <textarea
+                name="failure"
+                required
+                className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                onChange={inputHandler}
+                value={newOrder.failure}
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Presupuesto:
+              </label>
+              <input
+                type="number"
+                id="price"
+                name="price"
+                required
+                className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                onChange={inputHandler}
+                value={newOrder.price}
+              />
             </div>
           </div>
-          <div className="mb-2">
-            <label
-              htmlFor="failure"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Falla:
-            </label>
-            <input
-              type="text"
-              id="failure"
-              name="failure"
-              required
-              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-              onChange={inputHandler}
-              value={newOrder.failure}
-            />
+
+          <div>
+            <div className="mb-2">
+              <label
+                htmlFor="vehiclePlate"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Patente:
+              </label>
+              <div className="w-full inline-flex items-center">
+                <select
+                  className="w-full mt-1 py-2 px-3 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded"
+                  onChange={plateSelectHandler}
+                >
+                  <option value="">Seleccionar vehículo</option>
+                  {selectedClient ? (
+                    selectedClient.vehicles?.length === 0 ? (
+                      <option disabled>
+                        El cliente no tiene vehículos agregados.
+                      </option>
+                    ) : (
+                      selectedClient.vehicles[0]?.map((item) => (
+                        <option key={item.plate} value={item.plate}>
+                          {item.plate}
+                        </option>
+                      ))
+                    )
+                  ) : (
+                    <option disabled>No hay cliente seleccionado.</option>
+                  )}
+                </select>
+              </div>
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="estimateSolution"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Solucion Propuesta:
+              </label>
+              <textarea
+                name="estimateSolution"
+                required
+                className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                onChange={inputHandler}
+                value={newOrder.estimateSolution}
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="observations"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Observaciones:
+              </label>
+              <textarea
+                name="observations"
+                className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                onChange={inputHandler}
+                value={newOrder.observations}
+              />
+            </div>
           </div>
-          <div className="mb-2">
-            <label
-              htmlFor="estimateSolution"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Solucion Propuesta:
-            </label>
-            <input
-              type="text"
-              id="estimateSolution"
-              name="estimateSolution"
-              required
-              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-              onChange={inputHandler}
-              value={newOrder.estimateSolution}
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="price"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Presupuesto:
-            </label>
-            <input
-              type="number"
-              id="price"
-              name="price"
-              required
-              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-              onChange={inputHandler}
-              value={newOrder.price}
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="observations"
-              className="block text-sm font-medium text-gray-300"
-            >
-              Observaciones:
-            </label>
-            <input
-              type="text"
-              id="observations"
-              name="observations"
-              className="mt-1 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-              onChange={inputHandler}
-              value={newOrder.observations}
-            />
-          </div>
-          <div className="flex">
+
+          <div className="col-span-2 flex justify-between mt-2">
             <button
-              type="button"
-              className="w-full m-2 py-2 px-4 bg-gray-600 text-white font-semibold rounded-md hover:bg-gray-700 transition duration-200"
               onClick={cancelHandler}
+              className="mr-2 bg-red-800 hover:bg-[#72241d] text-white px-4 py-2 rounded"
             >
               Cancelar
             </button>
             <button
               type="submit"
-              className="w-full m-2 py-2 px-4 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition duration-200"
+              className="bg-green-600 hover:bg-[#1a7742] text-white px-4 py-2 rounded"
             >
               Crear orden
             </button>
--- src/app/components/Forms/CreateVehicleOnClientForm.tsx
+++ src/app/components/Forms/CreateVehicleOnClientForm.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { URL } from "../../../../config/consts";
 
 const CreateVehicleOnClientForm = ({
-  isOpen,
-  setIsOpen,
-  onClose,
+  isPopupOpen,
+  setIsPopupOpen,
   addVehicle,
 }) => {
   const [vehicle, setVehicle] = useState({
     plate: "",
     brand: "",
     modelo: "",
+    engine: "",
+    kilometers: "",
     year: "",
     details: "",
   });
@@ -19,109 +21,199 @@ const CreateVehicleOnClientForm = ({
     setVehicle({ ...vehicle, [name]: value });
   };
 
-  const handleSubmit = () => {
+  /* const handleSubmit = () => {
     addVehicle(vehicle);
-    onClose(); // Cierra el popup después de agregar el vehículo
+    onClose();
     setVehicle({
       plate: "",
       brand: "",
       modelo: "",
+      engine: "",
+      kilometers: "",
       year: "",
       details: "",
     });
+  }; */
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${URL}/vehicles`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+        body: JSON.stringify(vehicle),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al crear nuevo vehículo.");
+      }
+
+      const data = await response.json();
+      await addVehicle(data.data);
+
+      setVehicle({
+        plate: "",
+        brand: "",
+        modelo: "",
+        engine: "",
+        kilometers: "",
+        year: "",
+        details: "",
+      });
+      swal("Vehículo creado correctamente!", "", "success");
+      setIsPopupOpen(false);
+    } catch (error) {
+      swal(
+        "Error al crear nuevo vehículo.",
+        "Por favor intente nuevamente.",
+        "error"
+      );
+    }
   };
 
   useEffect(() => {
     const onEscClose = (e) => {
       if (e.key === "Escape") {
-        if (isOpen) {
-          setIsOpen(false);
+        if (isPopupOpen) {
+          setIsPopupOpen(false);
         }
       } else if (e.key === "Enter") {
         handleSubmit();
       }
     };
-    if (isOpen) {
+    if (isPopupOpen) {
       document.addEventListener("keydown", onEscClose);
     }
     return () => document.removeEventListener("keydown", onEscClose);
-  }, [isOpen]);
+  }, [isPopupOpen]);
 
-  if (!isOpen) return null;
+  if (!isPopupOpen) return null;
 
   return (
     <div className="mt-14 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-[#2d2c2d] bg-opacity-100 p-6 rounded-lg shadow-md w-96">
-        <h2 className="text-lg font-bold mb-4">Agregar Vehículo</h2>
+        <h2 className="flex items-center justify-center text-lg font-bold mb-4">
+          Agregar Vehículo
+        </h2>
         <form onSubmit={handleSubmit}>
-          <label
-            htmlFor="plate"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Patente:
-          </label>
-          <input
-            type="text"
-            name="plate"
-            className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-            onChange={handleChange}
-            value={vehicle.plate}
-            required
-          />
-          <label
-            htmlFor="brand"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Marca:
-          </label>
-          <input
-            type="text"
-            name="brand"
-            className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-            onChange={handleChange}
-            value={vehicle.brand}
-            required
-          />
-          <label
-            htmlFor="model"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Modelo:
-          </label>
-          <input
-            type="text"
-            name="modelo"
-            className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-            onChange={handleChange}
-            value={vehicle.modelo}
-            required
-          />
-          <label
-            htmlFor="year"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Año:
-          </label>
-          <input
-            type="text"
-            name="year"
-            className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-            onChange={handleChange}
-            value={vehicle.year}
-          />
-          <label
-            htmlFor="details"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Detalles:
-          </label>
-          <textarea
-            name="details"
-            className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-            onChange={handleChange}
-            value={vehicle.details}
-          />
-          <div className="mt-4 -mb-4 flex">
+          <div className="grid grid-cols-2 gap-4 w-full">
+            <div>
+              <div className="mb-2">
+                <label
+                  htmlFor="plate"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Patente:
+                </label>
+                <input
+                  type="text"
+                  name="plate"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.plate}
+                  required
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="brand"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Marca:
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.brand}
+                  required
+                />
+              </div>
+
+              <div className="mb-2">
+                <label
+                  htmlFor="engine"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Motor:
+                </label>
+                <input
+                  type="text"
+                  name="engine"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.engine}
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <label
+                  htmlFor="year"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Año:
+                </label>
+                <input
+                  type="number"
+                  name="year"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.year}
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="model"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Modelo:
+                </label>
+                <input
+                  type="text"
+                  name="modelo"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.modelo}
+                  required
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="kilometers"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Kilómetros:
+                </label>
+                <input
+                  type="number"
+                  name="kilometers"
+                  className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                  onChange={handleChange}
+                  value={vehicle.kilometers}
+                />
+              </div>
+            </div>
+            <div className="-mt-2 col-span-2">
+              <label
+                htmlFor="details"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Detalles:
+              </label>
+              <textarea
+                name="details"
+                className="mt-1 mb-2 block w-full p-2 border text-black border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+                onChange={handleChange}
+                value={vehicle.details}
+              />
+            </div>
+          </div>
+          {/* <div className="flex justify-between -mb-4">
             <button
               onClick={onClose}
               className="w-full m-2 py-2 px-4 bg-gray-600 text-white font-semibold rounded-md hover:bg-gray-700 transition duration-200"
@@ -133,6 +225,20 @@ const CreateVehicleOnClientForm = ({
               className="w-full m-2 py-2 px-4 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition duration-200"
             >
               Agregar
+            </button>
+          </div> */}
+          <div className="flex justify-between mt-4">
+            <button
+              onClick={() => setIsPopupOpen(false)}
+              className="mr-2 bg-red-800 hover:bg-[#72241d] text-white px-4 py-2 rounded"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="bg-green-600 hover:bg-[#1a7742] text-white px-4 py-2 rounded"
+            >
+              Confirmar
             </button>
           </div>
         </form>
